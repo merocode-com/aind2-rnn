2b21c3a52983cda4422af11167f405b777b0acc0
Elmansy, Amira <amira.elmansy@here.com>
2018-02-17 18:11:14 +0100

    Setting up RNN for Sequence generator

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 from functools import reduce
 
@@ -62,4 +63,9 @@ def window_transform_text(text, window_size, step_size):
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Activation('softmax'))
+    return model
+
